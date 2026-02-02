5228dab46594df0041ae0cb367c0915b6a48a08d
Ulysse HOLZINGER <ulysseholzinger@gmail.com>
2026-02-02 15:05:57 +0100

    base items

--- db/init.sql
+++ db/init.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS items (
 CREATE INDEX IF NOT EXISTS idx_items_name ON items(name);
 CREATE INDEX IF NOT EXISTS idx_items_price ON items(price);
 CREATE INDEX IF NOT EXISTS idx_items_created_at ON items(created_at);
+
+-- Insert default potions
+INSERT INTO items (id, name, effect, price, created_at) VALUES 
+  ('0', 'Skibidi Potion', '{"HP": 40}', 50, NOW()),
+  ('1', 'La Gourde du Brave', '{"Attack": 15}', 75, NOW())
+ON CONFLICT (id) DO NOTHING;
+
+
